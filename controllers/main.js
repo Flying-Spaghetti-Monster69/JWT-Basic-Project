9febbdb33555c8ed1e1b266d4eb32b51123bdfe7
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2022-12-29 21:22:15 -0500

    dashboard authentication addded

--- controllers/main.js
+++ controllers/main.js
@@ -12,15 +12,28 @@ const login = async (req,res) =>{
 
     const token = jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:'30d'})
 
-    console.log(token)
-    console.log(process.env.JWT_SECRET)
-    console.log(id)
     res.status(200).json({msg:'user created',token})
 }
 
 const dashboard = async (req,res) =>{
-    const luckyNumber = Math.floor(Math.random()*101);
-    res.status(200).json({msg:'hello, joema ma',secret:`heres is a your funny number = ${luckyNumber}`})
+    const authHeader = req.headers.authorization;
+    console.log(authHeader)
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new CustomAPIError('no token provided', 401)
+    }
+
+    const token = authHeader.split(' ')[1]
+    console.log(token)
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        console.log(decoded)
+        const luckyNumber = Math.floor(Math.random()*101);
+
+        res.status(200).json({msg:`welcome, ${decoded.username}`,secret:`heres is a your funny number = ${luckyNumber}`})
+    
+    } catch (error) {
+        throw new CustomAPIError('not authorized to access this route',401)
+    }
 }
 
 module.exports ={
